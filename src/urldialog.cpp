b6023fafee8a0d65560b6622c1f54eeb8795df78
minggang.li@gmail.com <minggang.li@gmail.com>
2018-01-07 18:46:18 +0800

    Fix: Change URL dialog to indicate it supports Magnet URI

--- src/urldialog.cpp
+++ src/urldialog.cpp
@@ -45,18 +45,17 @@ UrlDialog::UrlDialog(wxWindow* parent,wxWindowID id,const wxPoint& pos,const wxS
 	wxStaticText* StaticText1;
 	wxStdDialogButtonSizer* StdDialogButtonSizer1;
 
-	Create(parent, id, _("Enter Torrent URL/Magnet URI"), wxDefaultPosition, wxDefaultSize, wxDEFAULT_DIALOG_STYLE, _T("id"));
+	Create(parent, id, _("Enter Torrent URL or Magnet URI"), wxDefaultPosition, wxDefaultSize, wxDEFAULT_DIALOG_STYLE, _T("id"));
 	SetClientSize(wxDefaultSize);
 	Move(wxDefaultPosition);
 	BoxSizer1 = new wxBoxSizer(wxHORIZONTAL);
 	Panel1 = new wxPanel(this, wxID_ANY, wxDefaultPosition, wxDefaultSize, wxTAB_TRAVERSAL, _T("wxID_ANY"));
-	FlexGridSizer1 = new wxFlexGridSizer(2, 2, 0, 0);
-	StaticText1 = new wxStaticText(Panel1, wxID_ANY, _("URL:"), wxDefaultPosition, wxDefaultSize, 0, _T("wxID_ANY"));
+	FlexGridSizer1 = new wxFlexGridSizer(0, 1, 0, 0);
+	StaticText1 = new wxStaticText(Panel1, wxID_ANY, _("URL or Magnet URI:"), wxDefaultPosition, wxDefaultSize, 0, _T("wxID_ANY"));
 	FlexGridSizer1->Add(StaticText1, 1, wxALL|wxALIGN_LEFT|wxALIGN_TOP, 5);
 	m_textURL = new wxTextCtrl(Panel1, wxID_ANY, wxEmptyString, wxDefaultPosition, wxDefaultSize, wxTE_MULTILINE|wxVSCROLL, wxDefaultValidator, _T("wxID_ANY"));
 	m_textURL->SetMinSize(wxSize(380,-1));
 	FlexGridSizer1->Add(m_textURL, 1, wxALL|wxALIGN_LEFT|wxALIGN_CENTER_VERTICAL, 5);
-	FlexGridSizer1->Add(-1,-1,1, wxALL|wxALIGN_CENTER_HORIZONTAL|wxALIGN_CENTER_VERTICAL, 5);
 	StdDialogButtonSizer1 = new wxStdDialogButtonSizer();
 	StdDialogButtonSizer1->AddButton(new wxButton(Panel1, wxID_OK, wxEmptyString));
 	StdDialogButtonSizer1->AddButton(new wxButton(Panel1, wxID_CANCEL, wxEmptyString));
